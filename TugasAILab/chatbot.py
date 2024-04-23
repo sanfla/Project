d32651e0c89c69ef88aedfea2772117fb0914e15
Bagas <112615174+sanfla@users.noreply.github.com>
2024-04-24 00:58:34 +0700

    Update chatbot.py

--- TugasAILab/chatbot.py
+++ TugasAILab/chatbot.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import numpy as np
 import nltk
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('wordnet')
 import re
 import streamlit as st
 from sklearn.feature_extraction.text import TfidfVectorizer
